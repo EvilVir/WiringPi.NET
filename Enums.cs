9f8cda74b5fa9359dde53883cdb504856a14ef78
Kuba Pilecki <k.pilecki@gmail.com>
2016-08-27 23:49:45 +0200

    V1

---
+++ Enums.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WiringPiNet
+{
+    public enum PinMode : int
+    {
+        Input = 0,
+        Output = 1,
+        PwmOutput = 2,
+        GpioClock = 3,
+        SoftPwmOutput = 4,
+        SoftToneOutput = 5,
+        PwmToneOutput = 6
+    }
+
+    public enum PinValue : int
+    {
+        Low = 0,
+        High = 1,
+    }
+
+    public enum PullMode : int
+    {
+        Off = 0,
+        Down = 1,
+        Up = 2,
+    }
+
+    public enum PwmMode : int
+    {
+        Ms = 0,
+        Bal = 1,
+    }
+
+    public enum InterruptLevel : int
+    {
+        EdgeSetup = 0,
+        EdgeFalling = 1,
+        EdgeRising = 2,
+        EdgeBoth = 3,
+    }
+
+    public enum BoardModel : int
+    {
+        Unknown = 0,
+        PiA = 1,
+        PiB = 2,
+        PiBP = 3,
+        PiCM = 4,
+        PiAP = 5,
+        OdroidC = 6,
+        OdroidXU34 = 7,
+    }
+
+    public enum BoardVersion : int
+    {
+        Unknown = 0,
+        Version1 = 1,
+        Version11 = 2,
+        Version12 = 3,
+        Version2 = 4,
+    }
+
+    public enum BoardMaker : int
+    {
+        Unknown = 0,
+        EgoMan = 1,
+        Sony = 2,
+        Quisda = 3,
+        HardKernel = 4,
+    }
+}
---
+++ Wrapper/SPI.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WiringPiNet.Wrapper
+{
+    public class SPI
+    {
+        [DllImport("libwiringPi.so", EntryPoint = "wiringPiSPIGetFd")]
+        public static extern int WiringPiSPIGetFd(int channel);
+
+        [DllImport("libwiringPi.so", EntryPoint = "wiringPiSPIDataRW")]
+        public static extern int WiringPiSPIDataRW(int channel, byte[] data, int len);
+
+        [DllImport("libwiringPi.so", EntryPoint = "wiringPiSPISetup")]
+        public static extern int WiringPiSPISetup(int channel, int speed);
+    }
+}
